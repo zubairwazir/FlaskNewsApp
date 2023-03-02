82ec62b05b0d7151082ec3b1f6f0da224dc39a31
zubairwazir <zubairwazir777@gmail.com>
2023-03-02 22:44:13 +0500

    updating api keys

--- app.py
+++ app.py
@@ -4,12 +4,12 @@ import requests
 
 app = Flask(__name__)
 
-
 BASE_URL = 'https://www.reddit.com/'
 OAUTH_URL = 'https://oauth.reddit.com'
 
 with open('psd.txt', 'r') as f:
     psd = f.read()
+
 
 def get_token():
     data = {'grant_type': 'password', 'username': "zubairwazir", 'password': psd}
@@ -21,17 +21,19 @@ def get_token():
     app_name = 'FlaskNewsApp'
 
     r = requests.post(BASE_URL + 'api/v1/access_token',
-                    data=data,
-                    headers={'user-agent': app_name},
-                    auth=auth
-                    )
+                      data=data,
+                      headers={'user-agent': app_name},
+                      auth=auth
+                      )
     d = r.json()
     if "access_token" not in d:
         return {}
     token = 'bearer ' + d['access_token']
     return {'Authorization': token, 'User-Agent': app_name}
 
+
 HEADERS = get_token()
+
 
 @app.route('/')
 def index():
@@ -39,6 +41,7 @@ def index():
         return render_template('index.html')
     except Exception as e:
         return internal_server_error(e)
+
 
 @app.route('/reddit_news')
 def reddit_news():
@@ -58,15 +61,16 @@ def reddit_news():
 
         news_list = zip(headline, link, source, img)
 
-        return render_template('index.html', context = news_list)
+        return render_template('index.html', context=news_list)
 
     return unauthorized_error(Exception)
+
 
 @app.route('/search_reddit', methods=['POST'])
 def search_reddit():
     if HEADERS:
         q = request.form['keyword']
-        response = requests.get(OAUTH_URL + '/r/' + q +'/new', headers=HEADERS)
+        response = requests.get(OAUTH_URL + '/r/' + q + '/new', headers=HEADERS)
 
         headline = []
         link = []
@@ -81,7 +85,7 @@ def search_reddit():
 
         news_list = zip(headline, link, source, img)
 
-        return render_template('index.html', context = news_list)
+        return render_template('index.html', context=news_list)
 
     return unauthorized_error(Exception)
 
@@ -89,18 +93,17 @@ def search_reddit():
 @app.route('/newsapi_news')
 def newsapi_news():
     try:
-        newsapi = NewsApiClient(api_key="660bf94fd55d4391bd7166337fb35ae6")
+        newsapi = NewsApiClient(api_key="2c9ae45aceea424cb9e11eab17ee00a7")
     except Exception as e:
         return unauthorized_error(e)
 
     try:
         topheadlines = newsapi.get_top_headlines(
-                                        #q=keyword   #optional you can search by any keyword
-                                        #sources='bbc-news,the-verge',#optional and you can change
-                                        #category='business', #optional and you can change also
-                                        language='en', #optional and you can change also
-                                        country='in')
-
+            # q=keyword   #optional you can search by any keyword
+            # sources='bbc-news,the-verge',#optional and you can change
+            # category='business', #optional and you can change also
+            language='en',  # optional and you can change also
+            country='in')
 
         news = topheadlines['articles']
 
@@ -109,57 +112,52 @@ def newsapi_news():
         source = []
         img = []
 
-
         for i in range(len(news)):
-          news_list = news[i]
-        
+            news_list = news[i]
+
         headline.append(news_list['title'])
         link.append(news_list['url'])
         source.append(news_list['source']['name'])
         img.append(news_list['urlToImage'])
 
-
         news_list = zip(headline, link, source, img)
-        return render_template('index.html', context = news_list)
+        return render_template('index.html', context=news_list)
     except Exception as e:
         misunderstood_error(e)
-        
 
-    
-@app.route('/search_newsapi',methods=['POST']) 
+
+@app.route('/search_newsapi', methods=['POST'])
 def search_newsapi():
-    keyword = request.form['keyword']  #getting input from user
-    
+    keyword = request.form['keyword']  # getting input from user
+
     try:
-        newsapi = NewsApiClient(api_key="660bf94fd55d4391bd7166337fb35ae6")
+        newsapi = NewsApiClient(api_key="2c9ae45aceea424cb9e11eab17ee00a7")
     except Exception as e:
         return unauthorized_error(e)
 
     try:
         news = newsapi.get_top_headlines(q=keyword,
-                                        #sources='bbc-news,the-verge',#optional and you can change
-                                        #category='business', #optional and you can change also
-                                        language='en', #optional and you can change also
-                                        country='in')
+                                         # sources='bbc-news,the-verge',#optional and you can change
+                                         # category='business', #optional and you can change also
+                                         language='en',  # optional and you can change also
+                                         country='in')
         news = news['articles']
-        
+
         headline = []
         link = []
         source = []
         img = []
 
-
         for i in range(len(news)):
             news_list = news[i]
-        
+
         headline.append(news_list['title'])
         link.append(news_list['url'])
         source.append(news_list['source']['name'])
         img.append(news_list['urlToImage'])
 
-
         news_list = zip(headline, link, source, img)
-        return render_template('index.html', context = news_list)
+        return render_template('index.html', context=news_list)
     except Exception as e:
         return misunderstood_error(e)
 
